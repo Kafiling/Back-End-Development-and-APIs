5277ef14d60a88a4965900d9a5a0a1ed9df9dc67
Kafiling <panthawit.k@gmail.com>
2025-12-12 22:58:30 +0700

    Readable Streams

---
+++ 2-how-node-works/streams.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const server = require('http').createServer()
+
+server.on('request',(req,res) => {
+    //solution 1 : load enitre file into memory (BAD)
+    /*
+    fs.readFile('test-file.txt',(err,data) => {
+        if (err){
+            console.log(err)
+        }
+        res.end(data)
+    })
+    */
+
+    //solution 2 : use Readable Streams
+    const readableStream = fs.createReadStream('test-file.txt')
+    readableStream.on('data', chunk => {
+        //The respond will be writeable Stream
+        res.write(chunk)
+    })
+
+})
+
+//open server
+server.listen(8000,"127.0.0.1", () => {
+    console.log("Listening...")
+})
